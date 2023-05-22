546e6d24aab248bbad128c3fadcb17789f186752
Dongjie Xie <dxie@tenstorrent.com>
2023-05-22 09:30:47 -0500

    added Small|MediumBoomVecCosimConfig in Chipyard config-mixin

--- generators/chipyard/src/main/scala/config/BoomConfigs.scala
+++ generators/chipyard/src/main/scala/config/BoomConfigs.scala
@@ -17,6 +17,11 @@ class SmallBoomConfig extends Config(
   new chipyard.config.AbstractConfig)
 
 class SmallBoomVecConfig extends Config(
+  new boom.common.WithVector(1) ++                               // Add vector 
+  new boom.common.WithNSmallBooms(1) ++                          // small boom config
+  new chipyard.config.AbstractConfig)
+
+class SmallBoomVecCosimConfig extends Config(
   new boom.common.WithBoomDebugHarness ++                        // Enable debug harness
   new WithCustomBootROM ++                                       // Use custom BootROM to enable COSIM
   new boom.common.WithVector(1) ++                               // Add vector 
@@ -28,6 +33,11 @@ class MediumBoomConfig extends Config(
   new chipyard.config.AbstractConfig)
 
 class MediumBoomVecConfig extends Config(
+  new boom.common.WithVector(2) ++                               // Add vector 
+  new boom.common.WithNMediumBooms(1) ++                         // medium boom config
+  new chipyard.config.AbstractConfig)
+
+class MediumBoomVecCosimConfig extends Config(
   new boom.common.WithBoomDebugHarness ++                        // Enable debug harness
   new WithCustomBootROM ++                                       // Use custom BootROM to enable COSIM
   new boom.common.WithVector(2) ++                               // Add vector 
