a54cd0afae725aa0c5c598bd2ee58448df6f0f29
Dongjie Xie <dxie@tenstorrent.com>
2023-06-23 10:32:37 -0500

    1. Added BobcatConfigs 2. Updated ms1_vmv_vi 3. Bumped boom and rocket-chip to pickup initial Ovi changes

--- common.mk
+++ common.mk
@@ -337,7 +337,16 @@ ifeq (,$(WHISPER))
 	$(error WHISPER variable is not set. Set it to the path to whisper executable.)
 endif
 
-WHISPER_JSON = /root/my-chipyard/sims/cosim/bridge/whisper/config/boom.json
+ifneq (,$(findstring Ocelot,$(CONFIG)))
+  WHISPER_JSON = /root/my-chipyard/sims/cosim/bridge/whisper/config/ocelot.json
+else
+  ifneq (,$(findstring Bobcat,$(CONFIG)))
+    WHISPER_JSON = /root/my-chipyard/sims/cosim/bridge/whisper/config/bobcat.json
+  else
+    WHISPER_JSON = /root/my-chipyard/sims/cosim/bridge/whisper/config/boom.json
+  endif
+endif
+
 BOOTCODE = /root/my-chipyard/sims/cosim/bootrom/bootrom
 
 $(binary_hex): $(firstword $(BINARY)) | $(output_dir)
