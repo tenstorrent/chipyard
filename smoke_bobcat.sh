52c186c5f9a4e0bd34420929703dbe341376835f
kychen <kychen@tenstorrent.com>
2023-07-14 11:07:21 -0500

    update smoke test

--- smoke_bobcat.sh
+++ smoke_bobcat.sh
@@ -22,10 +22,12 @@ commands=(
   "make -C sims/vcs run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=$RISCV/riscv64-unknown-elf/share/riscv-tests/isa/rv64um-p-mul SIM_FLAGS=\"+cosim\""
   "make -C sims/vcs run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=$RISCV/riscv64-unknown-elf/share/riscv-tests/isa/rv64um-p-div SIM_FLAGS=\"+cosim\""
   "make -C sims/vcs run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=$RISCV/riscv64-unknown-elf/share/riscv-tests/isa/rv64uc-p-rvc SIM_FLAGS=\"+cosim\""
-  "make -C sims/vcs run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=/root/my-chipyard/tests/rvv/bringup_tests/ms3_vmv_xf.elf SIM_FLAGS=\"+cosim\""
   "make -C sims/vcs run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=/root/my-chipyard/tests/rvv/bringup_tests/ms1_vmv_vi.elf SIM_FLAGS=\"+cosim\""
   "make -C sims/vcs run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=/root/my-chipyard/tests/rvv/bringup_tests/ms2_vse64.elf SIM_FLAGS=\"+cosim\""
   "make -C sims/vcs run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=/root/my-chipyard/tests/rvv/bringup_tests/ms2p5_loadblock.elf SIM_FLAGS=\"+cosim\""
+  "make -C sims/vcs run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=/root/my-chipyard/tests/rvv/bringup_tests/ms3_vmv_xf.elf SIM_FLAGS=\"+cosim\""
+  "make -C sims/vcs run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=/root/my-chipyard/tests/rvv/bringup_tests/ms3p5_pureload.elf SIM_FLAGS=\"+cosim\""
+  "make -C sims/vcs run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=/root/my-chipyard/tests/rvv/bringup_tests/ms4_vle64.elf SIM_FLAGS=\"+cosim\""
 
 )
 
