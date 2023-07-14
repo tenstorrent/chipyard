d0f613f92b74d19bfdf10b536eb647b59c131f5e
kychen <kychen@tenstorrent.com>
2023-07-14 17:26:19 -0500

    remove other files

--- check.sh
+++
@@ -1,1 +0,0 @@
-make -C sims/vcs run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=/root/my-chipyard/tests/rvv/bringup_tests/ms2_vse64.elf SIM_FLAGS="+cosim" 
--- pureload.sh
+++
@@ -1,3 +0,0 @@
-#make -C sims/vcs run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=/root/my-chipyard/tests/rvv/bringup_tests/ms2_vse64.elf SIM_FLAGS="+cosim" 
-#make -C sims/vcs run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=/root/my-chipyard/tests/rvv/bringup_tests/ms2p5_loadblock.elf SIM_FLAGS="+cosim" 
-make -C sims/vcs run-binary-debug-hex CONFIG=SmallBobcatConfig BINARY=/root/my-chipyard/tests/rvv/bringup_tests/ms3p5_pureload.elf SIM_FLAGS="+cosim" 
